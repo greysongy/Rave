723a9fb061ff30bd0b4dc822e96018e05e59bf63
GarsonTech <garsontech@gmail.com>
2021-06-17 13:20:24 -0400

    Initial Commit

---
+++ Rave/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rave
+//
+//  Created by Bernie Cartin on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
